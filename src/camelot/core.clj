e119b962dcdda0cdf5e9abeaeadad55adb27c773
Kushal Pisavadia <kushi.p@gmail.com>
2012-04-09 14:08:01 +0100

    Can convert a font string into the equivalent ENUM
    Simple usage of a map key lookup.

--- src/camelot/core.clj
+++ src/camelot/core.clj
@@ -1,1 +1,23 @@
-(ns camelot.core)
+(ns camelot.core
+  (:import (org.apache.pdfbox.pdmodel.font PDType1Font)))
+
+(defonce font-map
+  {"Times-Roman"           PDType1Font/TIMES_ROMAN
+   "Times-Bold"            PDType1Font/TIMES_BOLD
+   "Times-Italic"          PDType1Font/TIMES_ITALIC
+   "Times-BoldItalic"      PDType1Font/TIMES_BOLD_ITALIC
+   "Helvetica"             PDType1Font/HELVETICA
+   "Helvetica-Bold"        PDType1Font/HELVETICA_BOLD
+   "Helvetica-Oblique"     PDType1Font/HELVETICA_OBLIQUE
+   "Helvetica-BoldOblique" PDType1Font/HELVETICA_BOLD_OBLIQUE
+   "Courier"               PDType1Font/COURIER
+   "Courier-Bold"          PDType1Font/COURIER_BOLD
+   "Courier-Oblique"       PDType1Font/COURIER_OBLIQUE
+   "Courier-BoldOblique"   PDType1Font/COURIER_BOLD_OBLIQUE
+   "Symbol"                PDType1Font/SYMBOL
+   "ZapfDingbats"          PDType1Font/ZAPF_DINGBATS})
+
+(defn font
+  "Given a string representing a font, returns the equivalent ENUM."
+  [^String name]
+  (font-map name))
--- test/camelot/test/core.clj
+++ test/camelot/test/core.clj
@@ -1,6 +1,20 @@
 (ns camelot.test.core
   (:use [camelot.core])
-  (:use [clojure.test]))
+  (:use [clojure.test])
+  (:import (org.apache.pdfbox.pdmodel.font PDType1Font)))
 
-(deftest replace-me ;; FIXME: write
-  (is false "No tests have been written."))
+(deftest converts-font-string-to-enum
+  (is (= PDType1Font/TIMES_ROMAN (font "Times-Roman")))
+  (is (= PDType1Font/TIMES_BOLD (font "Times-Bold")))
+  (is (= PDType1Font/TIMES_ITALIC (font "Times-Italic")))
+  (is (= PDType1Font/TIMES_BOLD_ITALIC (font "Times-BoldItalic")))
+  (is (= PDType1Font/HELVETICA (font "Helvetica")))
+  (is (= PDType1Font/HELVETICA_BOLD (font "Helvetica-Bold")))
+  (is (= PDType1Font/HELVETICA_OBLIQUE (font "Helvetica-Oblique")))
+  (is (= PDType1Font/HELVETICA_BOLD_OBLIQUE (font "Helvetica-BoldOblique")))
+  (is (= PDType1Font/COURIER (font "Courier")))
+  (is (= PDType1Font/COURIER_BOLD (font "Courier-Bold")))
+  (is (= PDType1Font/COURIER_OBLIQUE (font "Courier-Oblique")))
+  (is (= PDType1Font/COURIER_BOLD_OBLIQUE (font "Courier-BoldOblique")))
+  (is (= PDType1Font/SYMBOL (font "Symbol")))
+  (is (= PDType1Font/ZAPF_DINGBATS (font "ZapfDingbats"))))
