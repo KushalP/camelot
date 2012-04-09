20a48aadbdd7117d508718c860223a046806c954
Kushal Pisavadia <kushi.p@gmail.com>
2012-04-09 14:43:10 +0100

    Bump lein-clojars to 0.8.0

--- project.clj
+++ project.clj
@@ -2,4 +2,4 @@
   :description "A fast, nimble PDF generation and manipulation library"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.apache.pdfbox/pdfbox "1.6.0"]]
-  :dev-dependencies [[lein-clojars "0.6.0"]])
+  :dev-dependencies [[lein-clojars "0.8.0"]])
