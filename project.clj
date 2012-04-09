d5e9bfd76985a3695a1d8e87486b7e6579ff6270
Kushal Pisavadia <kushi.p@gmail.com>
2012-04-09 14:48:47 +0100

    Add license and url
    Make it more clear to those who peek inside of project.clj if they
    haven't grabbed it through GitHub.

--- project.clj
+++ project.clj
@@ -1,5 +1,8 @@
 (defproject camelot "0.1.1-SNAPSHOT"
   :description "A fast, nimble PDF generation and manipulation library"
+  :url "http://github.com/KushalP/camelot"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.apache.pdfbox/pdfbox "1.6.0"]]
   :dev-dependencies [[lein-clojars "0.8.0"]])
