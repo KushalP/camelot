cdd93f7b0fd9a7c8d15d0065074b0e62ea910cdc
Kushal Pisavadia <kushi.p@gmail.com>
2012-04-09 14:32:18 +0100

    Make it easier to push to clojars
    Also updated the wording of the description.

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject camelot "0.0.1-SNAPSHOT"
-  :description "Fast, Nimble PDF generation and manipulation library"
+  :description "A fast, nimble PDF generation and manipulation library"
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [org.apache.pdfbox/pdfbox "1.6.0"]])
+                 [org.apache.pdfbox/pdfbox "1.6.0"]]
+  :dev-dependencies [[lein-clojars "0.6.0"]])
