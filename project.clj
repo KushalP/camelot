96b8ae0b9177400b6078b2f340c7f59c85a17573
Kushal Pisavadia <kushi.p@gmail.com>
2012-05-02 12:24:28 +0100

    Move to 1.4.0
    Make sure we're using the latest stable version of Clojure.

--- project.clj
+++ project.clj
@@ -3,6 +3,6 @@
   :url "http://github.com/KushalP/camelot"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.3.0"]
+  :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.apache.pdfbox/pdfbox "1.6.0"]]
   :dev-dependencies [[lein-clojars "0.8.0"]])
